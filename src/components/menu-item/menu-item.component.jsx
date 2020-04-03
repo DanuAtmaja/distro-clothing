10612e695486b1e5f0e9921b1bea6ecac300151c
Danu <danuatmaja@gmail.com>
2020-04-03 10:39:38 +0800

    fixing menu-item styling

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import './menu-item.styles.scss';
 
 const MenuItem = ({title, imageUrl, size}) => (
+    <div className={`${size} menu-item`}>
     <div
-        className="background-image"
-        style={{
+      className='background-image'
+      style={{
         backgroundImage: `url(${imageUrl})`
       }}
-        className={`${size} menu-item`}>
+    />
         
         <div className="content">
             <h1 className="content__title">{title.toUpperCase()}</h1>

093755ff8de02c917182b2acb5fe692cbc8df193
Danu <danuatmaja@gmail.com>
2020-04-08 18:27:26 +0800

    collection overview component using redux

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+
+import Directory from '../../components/directory/directory.component';
+
 import './homepage.styles.scss';
 
-import Directory from '../../components/directory/directory.component.jsx';
-
-const Homepage = () => (
-  <div className="homepage">
+const HomePage = () => (
+  <div className='homepage'>
     <Directory />
   </div>
 );
 
-export default Homepage;
+export default HomePage;
